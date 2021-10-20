4c2d9842c969cad69d885ae123ca8bd65840fcf3
Harshpreet Singh <devharshpreet@gmail.com>
2021-10-20 14:53:13 +0530

    done

--- v1/accept_reject_request.php
+++ v1/accept_reject_request.php
@@ -4,32 +4,12 @@
     include('includes/config.php'); 
     $data = json_decode(file_get_contents('php://input'), true);
   
-    $first_name=$data['first_name'];
-    $last_name=$data['last_name'];
-    $email=$data['email'];
-    $phone=$data['phone'];
-    $longitude="";
-    $latitude="";
-    
-   $sql = "INSERT INTO users (parent_id, first_name, last_name, email, `password`, `cpassword`, phone, longitude, latitude )
-    VALUES ('','".$first_name."', '".$last_name."', '".$email."', '".$password."', '".$cpassword."', '".$phone."', '', '')";
+    if (isset($_POST['accept'])) {
+        #code here
+    }
 
-    if ($conn->query($sql) === TRUE) {
-        $dataArray=array();	  
-        $responseObj = new stdClass();
-        $responseObj->status = 'success';
-        $responseObj->message = 'Registration Successfull';
-        $responseObj->data = $dataArray; 	  
-        echo json_encode($responseObj);
-
-    } else {  
-        $dataArray=array();	  
-        $responseObj = new stdClass();
-        $responseObj->status = 'failed';
-        $responseObj->message = 'Email Already Exist';
-        $responseObj->data = $dataArray; 	  
-        echo json_encode($responseObj);
-
+    if (isset($_POST['decline'])) {
+        #code here
     }
 
 	 exit;
--- v1/cancel_request.php
+++ v1/cancel_request.php
@@ -4,34 +4,16 @@
     include('includes/config.php'); 
     $data = json_decode(file_get_contents('php://input'), true);
   
-    $first_name=$data['first_name'];
-    $last_name=$data['last_name'];
-    $email=$data['email'];
-    $phone=$data['phone'];
-    $longitude="";
-    $latitude="";
-    
-   $sql = "INSERT INTO users (parent_id, first_name, last_name, email, `password`, `cpassword`, phone, longitude, latitude )
-    VALUES ('','".$first_name."', '".$last_name."', '".$email."', '".$password."', '".$cpassword."', '".$phone."', '', '')";
-
-    if ($conn->query($sql) === TRUE) {
-        $dataArray=array();	  
-        $responseObj = new stdClass();
-        $responseObj->status = 'success';
-        $responseObj->message = 'Registration Successfull';
-        $responseObj->data = $dataArray; 	  
-        echo json_encode($responseObj);
-
-    } else {  
-        $dataArray=array();	  
-        $responseObj = new stdClass();
-        $responseObj->status = 'failed';
-        $responseObj->message = 'Email Already Exist';
-        $responseObj->data = $dataArray; 	  
-        echo json_encode($responseObj);
-
+    if (isset($_POST['accept'])) {
+        
     }
+   
 
-	 exit;
+
+
+
+    if (isset($_POST['cancel'])) {
+        
+    }
 ?>
 
